601f9bba36082b391987ba2de74127f98df3f1d6
Luís Miguel <luismigueldutraalves@gmail.com>
2024-05-30 22:15:09 -0300

    feat: create products and orders schema

---
+++ src/db/schema/order-items.ts
@@ -0,0 +1,36 @@
+import { createId } from '@paralleldrive/cuid2'
+import { text, pgTable, integer } from 'drizzle-orm/pg-core'
+import { orders } from './orders'
+import { products } from './products'
+import { relations } from 'drizzle-orm'
+
+export const orderItems = pgTable('order_items', {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  orderId: text('order_id')
+    .notNull()
+    .references(() => orders.id, {
+      onDelete: 'cascade',
+    }),
+  productId: text('product_id').references(() => products.id, {
+    onDelete: 'cascade',
+  }),
+  priceInCents: integer('price_in_cents').notNull(),
+  quantity: integer('quantity').notNull(),
+})
+
+export const orderItemsRelations = relations(orderItems, ({ one }) => {
+  return {
+    order: one(orders, {
+      fields: [orderItems.orderId],
+      references: [orders.id],
+      relationName: 'order_item_order',
+    }),
+    product: one(products, {
+      fields: [orderItems.productId],
+      references: [products.id],
+      relationName: 'order_item_product',
+    }),
+  }
+})
